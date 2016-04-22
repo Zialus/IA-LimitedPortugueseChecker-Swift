900241e88b8e90ed7df817dc222bef107e2f2133
Raul Ferreira <raulmendesferreira@gmail.com>
2016-04-22 23:57:47 +0100

    Initial Commit

---
+++ LimitedPortugueseChecker-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LimitedPortugueseChecker-Swift
+//
+//  Created by Raul Ferreira on 4/22/16.
+//  Copyright © 2016 FCUP. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
