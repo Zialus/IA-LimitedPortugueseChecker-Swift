9d9557769e8faa40d644dd179f638c3b2cc3adfc
Raul Ferreira <raulmendesferreira@gmail.com>
2016-04-24 19:42:51 +0100

    Allows the usage of "guard" to check for index out of bounds.

--- Sources/TugaChecker/Auxiliary.swift
+++ Sources/TugaChecker/Auxiliary.swift
@@ -15,3 +15,9 @@ func printfulldebug(string: Any) -> () {
     }
 }
 
+extension CollectionType {
+    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Generator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
