a954f0e9d2e6aeeaf16a746506025b55dbc210a8
Raul Ferreira <raulmendesferreira@gmail.com>
2017-10-22 16:05:45 +0100

    Updated Rainbow library.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TugaChecker",
     dependencies: [
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "2.0.0")
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0")
     ],
     targets: [
         .target(
