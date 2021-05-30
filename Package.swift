52b590c9d1eeddbe49b274e1774c0e5886b12b39
Raul Ferreira <raulmendesferreira@gmail.com>
2021-05-30 15:49:23 +0100

    Update Rainbow dependency

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "TugaChecker",
     dependencies: [
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0")
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.0")
     ],
     targets: [
         .target(
