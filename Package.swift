b3404b2cf2101b02c939c004037e11568d0bb9ee
Raul Ferreira <raulmendesferreira@gmail.com>
2025-05-10 17:03:11 +0000

    Update packages

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TugaChecker",
     dependencies: [
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.1")
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.1.0")
     ],
     targets: [
         .executableTarget(
